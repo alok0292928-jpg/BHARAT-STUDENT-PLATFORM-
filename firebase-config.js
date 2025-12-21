fc2fc8cdb34bba42e7df2d47340d1f903ecae476
alok0292928-jpg <alok0292928@gmail.com>
2025-12-21 15:37:43 +0530

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDmToqWOaBjODzAauhpxriCg-imiAKg-aQ",
+    authDomain: "bharat-student-platform.firebaseapp.com",
+    databaseURL: "https://bharat-student-platform-default-rtdb.firebaseio.com",
+    projectId: "bharat-student-platform",
+    storageBucket: "bharat-student-platform.firebasestorage.app",
+    messagingSenderId: "390311052094",
+    appId: "1:390311052094:web:93234b2311c1a44a87226f",
+    measurementId: "G-NDBKSP54N4"
+};
